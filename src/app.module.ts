7c45527e00aaa44ce1b19ada52b8edde15f39d97
Luis Sanchez Alcazar <lsancheza@vectoritcgroup.com>
2019-11-15 13:27:26 +0100

    cambio de las BD a Mongodb Atlas remote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { ProductosModule } from './productos/productos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/productos-nest',{ useNewUrlParser: true }), ProductosModule],
+  // imports: [MongooseModule.forRoot('mongodb://localhost/productos-nest',{ useNewUrlParser: true }), ProductosModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0-jsmjq.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true }), ProductosModule],
   controllers: [AppController],
   providers: [],
 })
